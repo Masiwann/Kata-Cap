03ae499f9d8cee70074f3530442658c03844824b
maitahme <maitahme@LFR018716.corp.capgemini.com>
2019-07-19 18:32:37 +0200

    Modify var name.

--- tennis/src/test/java/sprint2/Sprint2Test1.java
+++ tennis/src/test/java/sprint2/Sprint2Test1.java
@@ -6,42 +6,42 @@ import org.junit.Test;
 
 public class Sprint2Test1 {
 
-	sprint2.userstory1.Set tennisGame = new sprint2.userstory1.Set();
+	sprint2.userstory1.Set tennisSet = new sprint2.userstory1.Set();
 
 	@Test
 	public void testP1EasyWin(){
 		for(int i=0; i<(4*6);i++){ //4 points in a game, 6 games to win a set
-			tennisGame.p1Scores();
+			tennisSet.p1Scores();
 		}
 		
-		Assert.assertEquals("Player 1 WON the set !", tennisGame.getSetScore());
+		Assert.assertEquals("Player 1 WON the set !", tennisSet.getSetScore());
 	}
 
 	@Test
 	public void testP2EasyWin(){
 		for(int i=0; i<(4*6);i++){ //4 points in a game, 6 games to win a set
-			tennisGame.p2Scores();
+			tennisSet.p2Scores();
 		}
 		
-		Assert.assertEquals("Player 2 WON the set !", tennisGame.getSetScore());
+		Assert.assertEquals("Player 2 WON the set !", tennisSet.getSetScore());
 	}
 
 	@Test
 	public void testSpecialGame(){
 		for(int i=0; i<(4*5);i++){
-			tennisGame.p1Scores();
+			tennisSet.p1Scores();
 		}
 
 		for(int i=0; i<(4*6);i++){
-			tennisGame.p2Scores();
+			tennisSet.p2Scores();
 		}
 
 		//And now, the special Game
 		for(int i=0; i<7; i++){//7 points to win
-			tennisGame.p2Scores();
+			tennisSet.p2Scores();
 		}
 
-		Assert.assertEquals("Player 2 WON the set !", tennisGame.getSetScore());
+		Assert.assertEquals("Player 2 WON the set !", tennisSet.getSetScore());
 
 	}
 }
--- tennis/src/test/java/sprint2/Sprint2Test2.java
+++ tennis/src/test/java/sprint2/Sprint2Test2.java
@@ -6,42 +6,42 @@ import org.junit.Test;
 
 public class Sprint2Test2 {
 
-	sprint2.userstory2.Set tennisGame = new sprint2.userstory2.Set();
+	sprint2.userstory2.Set tennisSet = new sprint2.userstory2.Set();
 
 	@Test
 	public void testP1EasyWin(){
 		for(int i=0; i<(4*6);i++){ //4 points in a game, 6 games to win a set
-			tennisGame.p1Scores();
+			tennisSet.p1Scores();
 		}
 		
-		Assert.assertEquals("Player 1 WON the set !", tennisGame.getSetScore());
+		Assert.assertEquals("Player 1 WON the set !", tennisSet.getSetScore());
 	}
 
 	@Test
 	public void testP2EasyWin(){
 		for(int i=0; i<(4*6);i++){ //4 points in a game, 6 games to win a set
-			tennisGame.p2Scores();
+			tennisSet.p2Scores();
 		}
 		
-		Assert.assertEquals("Player 2 WON the set !", tennisGame.getSetScore());
+		Assert.assertEquals("Player 2 WON the set !", tennisSet.getSetScore());
 	}
 
 	@Test
 	public void testSpecialGame(){
 		for(int i=0; i<(4*5);i++){
-			tennisGame.p1Scores();
+			tennisSet.p1Scores();
 		}
 
 		for(int i=0; i<(4*6);i++){
-			tennisGame.p2Scores();
+			tennisSet.p2Scores();
 		}
 
 		//And now, the special Game
 		for(int i=0; i<7; i++){//7 points to win
-			tennisGame.p2Scores();
+			tennisSet.p2Scores();
 		}
 
-		Assert.assertEquals("Player 2 WON the set !", tennisGame.getSetScore());
+		Assert.assertEquals("Player 2 WON the set !", tennisSet.getSetScore());
 
 	}
 
@@ -49,29 +49,29 @@ public class Sprint2Test2 {
 	@Test
 	public void testTieBreak(){
 		for(int i=0; i<(4*5);i++){
-			tennisGame.p1Scores();
+			tennisSet.p1Scores();
 		}
 
 		for(int i=0; i<(4*6);i++){
-			tennisGame.p2Scores();
+			tennisSet.p2Scores();
 		}
 
 		//Currently 6-5
 		for(int i=0; i<7; i++){//7 points to win
-			tennisGame.p1Scores();
+			tennisSet.p1Scores();
 		}
 
 		//And now, the tiebreak game
 		for(int i=0; i<7; i++){//7 points to win
-			tennisGame.p1Scores();
+			tennisSet.p1Scores();
 		}
 
 		//Currenlty 7-6, not enough
 		for(int i=0; i<7; i++){//7 points to win
-			tennisGame.p1Scores();
+			tennisSet.p1Scores();
 		}
 
-		Assert.assertEquals("Player 1 WON the set !", tennisGame.getSetScore());
+		Assert.assertEquals("Player 1 WON the set !", tennisSet.getSetScore());
 
 
 	}
